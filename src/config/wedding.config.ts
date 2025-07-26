9c6e74090fc2e2d6f85c195c121d1cb0c5e9e91e
sarabagnoli <sarina.bagnoli@gmail.com>
2025-07-26 15:08:41 +0200

    Update wedding.config.ts

--- src/config/wedding.config.ts
+++ src/config/wedding.config.ts
@@ -2,8 +2,8 @@ import { WeddingConfigType } from "@/interfaces/config"
 
 const weddingConfig: WeddingConfigType = {
     people: {
-        bride: { Sara: 'Jane', lastName: ''},
-        groom: { Matteo: 'John', lastName: '' }
+        bride: { firstname: 'Sara', lastName: ''},
+        groom: { firstname: 'Matteo', lastName: '' }
     },
     date: {
         date: '2026-06-13',
@@ -11,8 +11,8 @@ const weddingConfig: WeddingConfigType = {
     },
     location: {
         title: 'Villa Malaspina',
-        address: 'Viale Cesare Battisti, 17 - Gualtieri (RE)',
-        link: 'https://maps.app.goo.gl/LLLW1QFHuo4SiXi9A,
+        address: 'Viale Cesare Battisti 17, Gualtieri (RE)',
+        link: 'https://maps.app.goo.gl/1dkBq49uxjo46brw8,
         latLng: '44.910002, 10.598130'
     }
 }
